119dfff4232d41ab034c838cc96d9dbaecaf0df1
beingharisali <theharis99@gmail.com>
2025-11-28 16:58:19 +0500

    updated backend url

--- services/http.ts
+++ services/http.ts
@@ -1,31 +1,32 @@
 import axios from "axios";
 
 const baseURL =
-	process.env.NEXT_PUBLIC_BACKEND_URL?.replace(/\/$/, "") ||
-	"http://localhost:5000/api";
+  process.env.NEXT_PUBLIC_BACKEND_URL?.replace(/\/$/, "") ||
+  "http://localhost:5000/api" ||
+  "https://full-stack-tfm-node.onrender.com";
 
 const http = axios.create({
-	baseURL,
-	timeout: 15000,
-	headers: {
-		"Content-Type": "application/json",
-	},
+  baseURL,
+  timeout: 15000,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 http.interceptors.request.use((config) => {
-	const token =
-		typeof window !== "undefined" ? localStorage.getItem("token") : null;
-	if (token) {
-		config.headers.Authorization = `Bearer ${token}`;
-	}
-	return config;
+  const token =
+    typeof window !== "undefined" ? localStorage.getItem("token") : null;
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 http.interceptors.response.use(
-	(response) => response,
-	(error) => {
-		return Promise.reject(error);
-	}
+  (response) => response,
+  (error) => {
+    return Promise.reject(error);
+  }
 );
 
 export default http;
